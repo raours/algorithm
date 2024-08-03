eb590f7d1db9f73329b0ed8b968e37b04b4a6849
raours <114200656+raours@users.noreply.github.com>
2024-08-03 20:35:21 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,16 +1,24 @@
+'''bj15903.py
+단순히 list를 쓰는 것은 정렬을 해줘야하기 때문에 비효율-> sort()의 시간 복잡도는 O(nlogn)
+headq를 사용하면 push만 하면 되기 때문에 효율적 -> heap에서 삽입 연산의 시간 복잡도는 O(logn)
+
+'''
+
 import sys
+import heapq
 input = sys.stdin.readline
+
 n, m = map(int, input().split())
 lst = list(map(int, input().split()))
+heapq.heapify(lst)
 
 
 for i in range(m):
-    lst.sort()
-    min1 = lst.pop(0)
-    min2 = lst.pop(0)
-    a = min1+min2
-    lst.append(a)
-    lst.append(a)
+    min1 = heapq.heappop(lst)
+    min2 = heapq.heappop(lst)
+    min_sum = min1+min2
 
+    heapq.heappush(lst, min_sum)
+    heapq.heappush(lst, min_sum)
 
 print(sum(lst))
