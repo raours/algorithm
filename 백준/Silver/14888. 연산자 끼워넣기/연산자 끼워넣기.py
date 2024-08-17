3fe2abac0cfda7c58b6465b85713cdf7bf64290b
raours <114200656+raours@users.noreply.github.com>
2024-08-18 01:47:02 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 160 ms, Memory: 113384 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,42 +1,38 @@
+
 N = int(input())
-
 A = list(map(int, input().split()))
-
 cal = list(map(int, input().split()))
-
 mx = -1e9
 mn = 1e9
 
-def dfs(n, temp) :
+
+def dfs(n, temp):
     global mx, mn
-    
-    # 종료 조건
-    if n == N-1:
+    if n == N-1: #끝에 도달하면 return 해서 백트래킹
         mx = max(temp, mx)
         mn = min(temp, mn)
         return
-     
-    # 하부함수 호출
-    if cal[0] != 0 : # 덧셈하는 경우
-        cal[0] -= 1
+    
+    if cal[0] !=0:
+        cal[0] -=1
         dfs(n+1, temp + A[n+1])
-        cal[0] += 1 
+        cal[0] +=1  #백트래킹
 
-    if cal[1] != 0 : # 뺄셈하는 경우
-        cal[1]-= 1
+    if cal[1] !=0:
+        cal[1] -=1
         dfs(n+1, temp - A[n+1])
-        cal[1] += 1
-    
-    if cal[2] != 0 : # 곱셈하는 경우
-        cal[2] -= 1
-        dfs(n+1, temp * A[n+1])
-        cal[2] += 1
-    
-    if cal[3] != 0 : #나눗셈하는 경우
-        cal[3] -= 1
-        dfs(n+1, int(temp/A[n+1]))
-        cal[3] += 1
+        cal[1] +=1
 
-dfs(0, A[0])
+    if cal[2] !=0:
+        cal[2] -=1
+        dfs(n+1, temp * A[n+1])
+        cal[2] +=1
+
+    if cal[3] !=0:
+        cal[3] -=1
+        dfs(n+1, int(temp/A[n+1]))
+        cal[3] +=1
+
+dfs(0,A[0])
 print(mx)
 print(mn)
