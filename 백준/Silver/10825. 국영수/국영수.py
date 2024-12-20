8419eccbfb706ecb3bb0f62e4bb2ff3f52ba7214
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2024-12-20 17:10:45 +0900

    [Silver IV] Title: 국영수, Time: 2940 ms, Memory: 65480 KB -BaekjoonHub

---
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -0,0 +1,11 @@
+
+T = int(input())
+lst = []
+for _ in range(T):
+    a,b,c,d = input().split()
+    lst.append([a,int(b),int(c),int(d)])
+
+
+lst.sort(key=lambda  x: (-x[1],x[2],-x[3],x[0]))
+for i in  range(T):
+    print(lst[i][0])
