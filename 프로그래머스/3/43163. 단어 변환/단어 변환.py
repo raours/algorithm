69983cbe9d6178334026a38a4bf2dd0ad30b5908
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-04-25 18:03:14 +0900

    [level 3] Title: 단어 변환, Time: 0.47 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,35 +1,51 @@
-from collections import deque
+# def dfs(begin, target, visit,words, dep):
+#     if begin == target:
+#         return dep
+#     for i in range(len(words)):
+#         if not visit[i]:
+#             cnt = 0
+#             for j in range(len(words[i])):
+#                 if words[i][j] != begin: 
+#                     cnt += 1
+#             if cnt == 1:
+#                 visit[i] = True
+#                 dfs(words[i],target,visit,words, dep+1)
+#                 visit[i] = False
+    
+
+
+# from collections import deque
+# def solution(begin, target, words):
+#     #변환 불가능하면 0 return
+#     if not target in words:
+#         return 0
+#     n = len(words)
+#     visit = [False] * n
+    
+#     ans = dfs(begin, target, visit, words, 0)
+#     print(ans)
+    
+    
+def dfs(begin, target, visit, words, depth, min_depth):
+    if begin == target:
+        min_depth[0] = min(min_depth[0], depth)
+        return
+
+    for i in range(len(words)):
+        if not visit[i]:
+            diff = sum(1 for a, b in zip(begin, words[i]) if a != b)
+            if diff == 1:
+                visit[i] = True
+                dfs(words[i], target, visit, words, depth + 1, min_depth)
+                visit[i] = False
+
+
 def solution(begin, target, words):
-    #변환 불가능하면 0 return
-    if not target in words:
+    if target not in words:
         return 0
-    
-    words.sort()
-    n = len(words)
-    answer = 0
-    q = deque()
-    q.append(begin)
-    visit = [False] * n
-    
-    while q:
-        before = q.popleft()
-        
-        if before == target:
-            return answer
-        
-        for i in range(n):
-            if not visit[i]: 
-                num = 0
-                after = words[i]
-                
-                for j in range(len(after)):
-                    if before[j] != after[j]:
-                        num += 1
-                if num == 1: # 문자 하나만 다르면, 변환 가능
-                    # print(after)
-                    q.append(after)
-                    visit[i] = True
-                    answer += 1
-                    break
-            
-    return 0
+
+    visit = [False] * len(words)
+    min_depth = [float('inf')]  # 리스트로 감싸야 참조 가능 (mutable)
+    dfs(begin, target, visit, words, 0, min_depth)
+
+    return 0 if min_depth[0] == float('inf') else min_depth[0]
