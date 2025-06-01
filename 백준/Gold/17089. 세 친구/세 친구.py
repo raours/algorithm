c8272e4b6e912403fbe236f4f5c01f392c808a9c
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-06-01 19:38:07 +0900

    [Gold V] Title: 세 친구, Time: 1412 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17089. 세 친구/세 친구.py
+++ 백준/Gold/17089. 세 친구/세 친구.py
@@ -2,19 +2,22 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-lst = [[] for _ in range(N+1)]
+lst = [set() for _ in range(N+1)]  # 리스트 → set
 
 for _ in range(M):
     x, y = map(int, input().split())
-    lst[x].append(y)
-    lst[y].append(x)
+    lst[x].add(y)
+    lst[y].add(x)
+
 answer = 1e9
-for flst in lst: #각 번호에 대한 친구리스트를 모두 돌며 (a)
-    for j in flst: # 그 번호에 대한 친구리스트 안의 한명을 선택 (b)
-        for k in lst[j]: # (c) b의 친구리스트에 있는 c를 한명 골라서,
-            if k in flst: #c가 a의 친구리스트에도 있다면, 셋은 친구!
-                answer = min(answer, len(flst)+len(lst[j])+len(lst[k])-6)
+for a in range(1, N+1):
+    for b in lst[a]:
+        for c in lst[b]:
+            if c in lst[a] and a < b < c:  # 중복 제거: a < b < c 조건 추가
+                total = len(lst[a]) + len(lst[b]) + len(lst[c]) - 6
+                answer = min(answer, total)
 
 if answer == 1e9:
     print(-1)
-else: print(answer)
+else:
+    print(answer)
