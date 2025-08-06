043b1e0126e82c3bd420e4dfc40ffe509499275e
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-07 01:51:31 +0900

    [Gold III] Title: 내리막 길, Time: 112 ms, Memory: 46040 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,38 +1,34 @@
+'''
+2025.8.6
+백준 : 내리막 길
+'''
 import sys
+
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**9)
+
 n, m = map(int, input().split())
-graph = []
+board = [list(map(int, input().split())) for _ in range(n)]
 
-moves = [
-    [1, 0],
-    [0, 1],
-    [-1, 0],
-    [0, -1]
-]
+dr = [0,1,0,-1]
+dc = [1,0,-1,0]
+dp = [[-1] * m for _ in range(n)]
 
-visited = [[-1] * m for _ in range(n)]
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
-
-def dfs(x, y):
-    # Base Case
-    if x == n - 1 and y == m - 1:
+def dfs(r,c):
+    # 종료 조건
+    if (r,c) == (n-1,m-1): #맨 끝칸 도달했으면 return
         return 1
-    
-    # 이미 방문한 곳일 때
-    if visited[x][y] != -1:
-        return visited[x][y]
 
-    visited[x][y] = 0
+    if dp[r][c] != -1: #이미 r,c 점부터 도착점까지의 경우의 수를 구해놨다면 바로 반환
+        return dp[r][c]
 
-    for move in moves:
-        nx, ny = x + move[0], y + move[1]
+    dp[r][c] = 0 # (r,c) 점부터 도착점까지의 경우의 수를 이제부터 구하겠다!
+    for i in range(4):
+        nr, nc = r+dr[i], c+dc[i]
+        if 0<=nr<n and 0<=nc<m and board[nr][nc] < board[r][c]:
+            dp[r][c] += dfs(nr,nc) #그 다음 지점으로부터 거슬러 올라가 또 도착점 까지의 경우의 수를 구함
+            #그걸 더해주며, 시작점까지 거슬러 올라오며 경우의 수를 갱신!
 
-        if 0 <= nx < n and 0 <= ny < m:
-            if graph[x][y] > graph[nx][ny]:
-                visited[x][y] += dfs(nx, ny)
+    return dp[r][c] #갱신한 경우의 수 반환!
 
-    return visited[x][y]
-
-print(dfs(0, 0))
+print(dfs(0,0))
