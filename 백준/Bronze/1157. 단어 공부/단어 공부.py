31cc0bb064de9ef0935693ede849612815c439f6
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-12 00:00:52 +0900

    [Bronze I] Title: 단어 공부, Time: 212 ms, Memory: 34368 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -2,12 +2,9 @@
 2025.8.11
 백준 : 단어  공부
 '''
-st = input().strip()
+st = input().upper()
 dic = {}
 for s in st:
-    if ord(s)>96: #소문자면
-        s = chr(ord(s)-32) #대문자로 바꿔주고
-
     if s in dic: #나온 적 있는 문자면
         dic[s] += 1
     else:
