990f2f9304d69f666849cd4024006d0d69b82cc0
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-14 00:28:55 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -1,6 +1,5 @@
-while True:
-    try:
-        print(input())
-    except EOFError:
-        break #try-except를 이용한 예외처리
-        
+import sys
+
+lines = sys.stdin.readlines() #파일의 끝 부분까지 한번에 가져옴
+for line in lines:
+    print(line.strip())
