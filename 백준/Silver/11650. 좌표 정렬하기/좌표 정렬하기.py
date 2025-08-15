045d84c82ce0e20d32c2869ae8b78d6a48b94d41
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-15 17:55:03 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2756 ms, Memory: 60360 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,13 @@
+'''
+2025.8.15
+'''
+import sys
+
+n = int(input())
+
+lst = [list(map(int, input().split())) for _ in range(n)]
+
+lst.sort(key = lambda x: (x[0], x[1]))
+
+for i in range(n):
+    print(*lst[i])
