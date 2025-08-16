3b019d770fce71a9622e22d1299ecc2cd8a12476
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-16 22:57:17 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,17 @@
+'''
+2025.8.16
+'''
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+lst = [num for num in range(1,n+1)]
+ans = []
+idx = 0
+while len(lst)>0:
+    idx += (k-1)
+    if idx >= len(lst): #리스트 범위 넘어가면,
+        idx = idx % len(lst)
+    ans.append(str(lst.pop(idx))) #그 인덱스 값 pop해서 ans에 붙여주기
+print('<'+', '.join(ans)+'>')
+
