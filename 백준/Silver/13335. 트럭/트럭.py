d04fcb17c23d99e169d5afd0eba2b3b43d3b02a8
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-17 18:31:43 +0900

    [Silver I] Title: 트럭, Time: 64 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -6,38 +6,22 @@ from collections import deque
 input = sys.stdin.readline
 
 n, w, l = map(int, input().split())
-truck = list(map(int, input().split()))
+truck = deque(map(int, input().split())) #대기 중인 트럭들
 t = 0
-q = deque()
-idx = 0
+q = deque([0]*w) # 다리 길이 만큼 0으로 채워 초기화
+weight = 0
 
-def cal_sum(que):
-    temp = 0
-    for i in range(len(que)):
-        temp += que[i][0]
-    return temp
-
-done = [0] * n
-
-def flag():
-    for i in range(n):
-        if done[i] == False:
-            return True
-    return False
-
-while flag():
+while truck or weight>0: #대기 중인 트럭이 있거나, 현재 다리 위에 트럭이 있으면 계속
     t += 1 #시간 증가
 
-    # 1. 도달한 트럭부터 빼기
-    #큐에넣은 시각+w == t 면 빼야함
-    if len(q)>0 and q[0][1]+w == t:
-        done[q[0][2]] = 1
-        q.popleft() # 건너감!
+    # 1. 도달한 트럭 or 빈칸 빼기
+    weight -= q.popleft()
 
-    if len(q)<w: #들어갈 자리가 있고,
-        if idx < n and cal_sum(q)+truck[idx] <= l: # 최대하중을 넘지 않는다면,
-            q.append((truck[idx],t,idx)) #무게, 들어온 시각
-            idx += 1
-
+    if truck and weight+truck[0] <= l: # 최대하중을 넘지 않는다면,
+        q.append(truck[0]) #트럭 다리위로
+        weight += truck[0]
+        truck.popleft() #대기 중인 트럭에서 제외
+    else:
+        q.append(0) # 트럭을 못 올리면, 빈칸을 채워, 다리 길이 유지
 
 print(t)
