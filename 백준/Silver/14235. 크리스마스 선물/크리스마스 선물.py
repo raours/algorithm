0870027e6b7a8644e8a0463c0171d98f78ebb3d3
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-20 22:58:42 +0900

    [Silver III] Title: 크리스마스 선물, Time: 112 ms, Memory: 42216 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
@@ -9,16 +9,13 @@ input = sys.stdin.readline
 n = int(input()) # 방문한 횟수
 hq = []
 for _ in range(n):
-    st = input()
-    if st[0] == '0':
+    lst = list(map(int, input().split()))
+    if lst[0] == 0:
         # 아이들 방문
         if len(hq) == 0:
             print(-1)
         else:
             print(-heapq.heappop(hq))
     else:
-        #거점지 방문
-        lst = list(map(int, st[2:].split()))
-
-        for a in lst:
+        for a in lst[1:]:
             heapq.heappush(hq, -a)
