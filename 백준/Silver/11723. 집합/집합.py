b56ea3ef99b1bc8dab0725040440743521a36e50
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-23 12:37:31 +0900

    [Silver V] Title: 집합, Time: 3460 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,34 +1,34 @@
 '''
 2025.8.23
+비트마스킹
 '''
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-s = set()
+bit = 0
 for _ in range(n):
     senten = input().split()
     if len(senten) > 1:
-        x,y = senten[0], int(senten[1])
+        x,y = senten[0], int(senten[1])-1
         if x == 'add':
-            s.add(y)
+            bit = bit | (1<<y)
         elif x == 'remove':
-            if y in s:
-                s.discard(y)
+            bit = bit & ~(1<<y)
+
         elif x == 'check':
-            if y in s:
-                print(1)
-            else:
+            if bit & (1<<y) == 0:
                 print(0)
-        elif x == 'toggle':
-            if y in s:
-                s.discard(y)
             else:
-                s.add(y)
+                print(1)
+        elif x == 'toggle':
+            if bit & (1<<y) == 0:
+                bit = bit | (1<<y)
+            else:
+                bit = bit & ~(1<<y)
     else:
         if senten[0] == 'all':
-            s = set(num for num in range(1,21))
+            bit = (1<<20) -1
 
         else:
-            s = set()
+            bit = 0
