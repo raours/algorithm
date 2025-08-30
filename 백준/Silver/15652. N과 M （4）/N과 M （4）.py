2bd6bab7957d4eed1e17c55bedbb1a9eb979492d
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-08-30 19:43:19 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,21 @@
+'''
+2025.8.30
+'''
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+
+s = []
+def dfs(start):
+
+        if len(s) == m: #모든 숫자 뽑았으면, return
+            print(' '.join(map(str,s))) #오름차순으로 탐색 중이라 바로 출력 가능
+            return
+
+        for i in range(start, n+1):
+            s.append(i)
+            dfs(i)
+            s.pop() #back tracking
+
+dfs(1)
