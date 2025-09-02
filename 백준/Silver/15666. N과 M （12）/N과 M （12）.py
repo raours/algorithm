acebfdf0aca7d3b16028ef3ea54ea69bcdacb4ad
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-03 00:45:31 +0900

    [Silver II] Title: N과 M (12), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -10,22 +10,17 @@ def dfs(start):
     if len(ans) == m: #n개 뽑았으면,
         print(*ans)
         return
-    prev = None #같은 depth에서 똑같은 숫자로 시작하는 건 pass
-    for i in range(start, n):
-        if lst[i] != prev:
-            ans.append(lst[i])
-            dfs(i) #같은 수 여러번 가능!
-            ans.pop()
-            prev = lst[i] #현재 depth에서 선택했던 수 저장
+    for i in range(start, len(lst)): #바뀐 길이만큼!
+        ans.append(lst[i])
+        dfs(i) #같은 수 여러번 가능!
+        ans.pop()
 
 
 n, m = map(int, input().split())
-lst = list(map(int,input().split()))
-lst.sort() #오름차순 정렬
+lst = sorted(set(map(int, input().split()))) #set으로 중복 제거 후 오름차순 정렬!
+#어차피, 숫자 중복 사용 가능하니까, 굳이 depth 맏 따질 필요x
 ans = []
 
 dfs(0)
-
-
 
 
