31d69af320b18651d01842afd552e3055e7bde7c
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-04 21:18:14 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -7,54 +7,26 @@ from collections import deque
 n, m = map(int, input().split())
 lst = [list(map(str, input().strip())) for _ in range(n)]
 
-dr = [0,1,0,-1]
-dc = [1,0,-1,0]
-
-def bfs(i,j,start):
-    temp = 0
-    # # 시작, W일때
-    # bfs(i, j, 'W')
-
-    visit = [[None] * m for _ in range(n)]  # 방문 체크
-
-    if start == 'W':
-        if lst[i][j] == 'B':
-            temp += 1  # 변경
-        visit[i][j] = 'W'  # 체크
-
-    if start == 'B':
-        if lst[i][j] == 'W':
-            temp += 1  # 변경
-        visit[i][j] = 'B'  # 체크
-
-    q = deque()
-    q.append((i, j))
-
-    while q:
-        sr, sc = q.popleft()
-
-        for k in range(4):
-            nr, nc = sr + dr[k], sc + dc[k]
-
-            if i <= nr < i + 8 and j <= nc < j + 8 and not visit[nr][nc]:
-                if visit[sr][sc] == lst[nr][nc]:  # 인접한 부분이 같으면, 변경
-                    temp += 1
-
-                # 방문처리 및 정보저장
-                if visit[sr][sc] == 'W':
-                    visit[nr][nc] = 'B'
-                else:
-                    visit[nr][nc] = 'W'
-                q.append((nr, nc))  # 큐에 삽입
-    return temp
-
-ans = 1e9
+ans = []
 for i in range(n-7): #시작점 고르기
     for j in range(m-7):
-        #시작, W일때
-        ans = min(ans, bfs(i,j,'W'))
+        white = 0 #시작, W일때
+        black = 0 #시작, B일때
 
-        #시작, B일때
-        ans = min(ans, bfs(i,j,'B'))
+        for r in range(i, i+8):
+            for c in range(j, j+8):
+                #좌표의 합이 홀,짝,홀,짝 이런 식으로 구성되어 있다!
+                if (r+c)%2 == 0: # (0,0)기준 W, B
+                    if lst[r][c] == 'B':
+                        white += 1 #W여야 하는데, B니까 +1
+                    else:
+                        black += 1 #B여야 하는데, W니까
+                else: # 좌표 합 홀수 인 곳~!
+                    if lst[r][c] == 'W':
+                        white += 1 #B여야 하는데, W니까 +1
+                    else:
+                        black += 1 #W여야 하는데, B니까
 
-print(ans)
+        ans.append(min(black, white))
+
+print(min(ans))
