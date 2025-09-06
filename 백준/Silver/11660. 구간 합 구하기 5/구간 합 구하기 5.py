24e187140b3459439fc0345976a49d9c4d9afba7
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-06 16:52:25 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1056 ms, Memory: 106832 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,4 +1,8 @@
+'''
+2025.9.5
+'''
 import sys
+
 input=sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -6,14 +10,10 @@ lst = [list(map(int, input().split())) for _ in range(n)]
 
 sum_lst = [[0] * (n + 1) for _ in range(n+1)]
 
-# 누합
+# 누적합 dp로 구하기
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        sum_lst[i][j] = sum_lst[i][j - 1] + lst[i-1][j - 1]
-
-for i in range(2, n + 1):
-    for j in range(1, n + 1):
-        sum_lst[i][j] += sum_lst[i - 1][j]
+        sum_lst[i][j] = sum_lst[i][j-1] + sum_lst[i-1][j]+lst[i-1][j-1]-sum_lst[i-1][j-1]
 
 for t in range(m):
     sr, sc, er, ec = map(int, input().split())
