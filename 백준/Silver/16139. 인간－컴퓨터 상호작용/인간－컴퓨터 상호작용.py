24fe83b11e9f5cc5c3780af766c261ad490d4f21
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-06 21:26:04 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 588 ms, Memory: 95976 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -11,15 +11,14 @@ n = int(input())
 
 # 같은 문자열로 질문을 계속 한다함 = st를 기준으로 계속 구하면 됨!
 # 누적합은 모든 알파벳 기준으로 2차원배열!
-lst = [[0]*(l+1) for _ in range(26)]
-for i in range(1, l+1):
-    for j in range(26):
-        if ord(st[i-1])-97 == j:
-            lst[j][i] = lst[j][i-1]+1
-        else:
-            lst[j][i] = lst[j][i-1]
+lst = [[0]*26 for _ in range(2)] #첫번째 줄 모든 알파벳의 0번째 기준 !
+lst[1][ord(st[0])-97] = 1 #첫번째 문자 체킹
+
+for i in range(2, l+1): #i번째의 문자에 대한 누적합 만들어서 append
+    lst.append(lst[i-1][:]) #복사해서 붙이기
+    lst[i][ord(st[i-1])-97] += 1 #해당 문자에 대한 개수 카운팅만!
 
 for _ in range(n):
     s, start, end = input().split()
     num = ord(s)-97 #아스키코드
-    print(lst[num][int(end)+1]-lst[num][int(start)])
+    print(lst[int(end)+1][num]-lst[int(start)][num])
