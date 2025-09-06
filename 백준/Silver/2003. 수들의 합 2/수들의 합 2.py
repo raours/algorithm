f156715608d3797ec5b4c8f346cf037088e52ed7
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-06 18:18:33 +0900

    [Silver IV] Title: 수들의 합 2, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -6,15 +6,17 @@ import sys
 input=sys.stdin.readline
 
 n, m = map(int, input().split())
-lst = list(map(int, input().split()))
+lst = [0] + list(map(int, input().split()))
+for i in range(2, n+1): #구간 합
+    lst[i] += lst[i-1]
 
-
+#투포인터
 right = 1
 left = 0
 ans = 0
 
 while right<= n and left <= right:
-    temp = sum(lst[left:right])
+    temp = lst[right] - lst[left]
     if temp <m:
         right += 1 #구간합 증가시키기
     elif temp > m:
