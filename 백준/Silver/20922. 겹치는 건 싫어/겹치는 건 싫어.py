6e6fc0f8063b0e3be92cf56d69b3676b8c890fee
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-16 03:40:20 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 292 ms, Memory: 53288 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.py
@@ -0,0 +1,29 @@
+'''
+2025.9.16
+'''
+import sys
+input=sys.stdin.readline
+
+n, k = map(int, input().split())
+lst = list(map(int, input().split()))
+
+cnt = [0] * 100001
+l = 0 #현재 길이
+
+ans = 0
+start = 0
+end = 0
+cur = 0
+
+while start<=end and end<n:
+    num = lst[end]
+    if cnt[num] < k:
+        cnt[num] += 1
+        cur += 1
+        end += 1
+    else:
+        cnt[lst[start]] -= 1
+        start += 1
+        cur -= 1
+    ans = max(ans, cur)
+print(ans)
