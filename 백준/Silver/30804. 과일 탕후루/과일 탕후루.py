50b5fa04deff8ab68f1f68e307273f67fc6ab3df
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-16 21:47:32 +0900

    [Silver II] Title: 과일 탕후루, Time: 248 ms, Memory: 35588 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.py
@@ -13,27 +13,23 @@ ans = 0
 start = 0
 end = 0
 cur = 0 #현재 과일 가짓 수
-l = 0
 while end<n:
     if cnt[lst[end]] == 0:  # 없던 종류가 추가되면,
         if cur < 2: #추가해도 되면,
             cur += 1
             cnt[lst[end]] += 1
             end += 1
-            l += 1
         else: #추가 안되면,
             cnt[lst[start]] -= 1
             if cnt[lst[start]] == 0:
                 cur -= 1
             start += 1
-            l -= 1
     else: #있던 종류 추가면,
         cnt[lst[end]] += 1
         end += 1
-        l += 1
-    ans = max(ans, l)
+    ans = max(ans, end-start)
 
-    
+
 print(ans)
 
 
