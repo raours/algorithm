c77ababfe516fa3cddde0237b942994d6a47c250
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-20 21:59:46 +0900

    [Silver II] Title: 연속합, Time: 80 ms, Memory: 39096 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,14 @@
+'''
+2025.9.20
+'''
+import sys
+input=sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().split()))
+
+for i in range(1, n):
+    lst[i] = max(lst[i], lst[i-1]+lst[i])
+
+print(max(lst))
+
