498ccd7bdb0ca425ab958cbc32a67fd3956744e2
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-09-21 18:20:57 +0900

    [Gold II] Title: 문제집, Time: 196 ms, Memory: 41656 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.py
@@ -0,0 +1,34 @@
+'''
+2025.9.21
+'''
+import sys
+import heapq
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+lst = [[] for _ in range(n+1)] #학생마다 뒤에 있는 학생정보
+con = [0]*(n+1) #진입차수 저장할 배열
+for _ in range(m):
+    x,y = map(int, input().split())
+    con[y] += 1 #진입차수 +1
+    lst[x].append(y)
+
+hq = []
+for i in range(1,n+1):
+    if con[i] == 0: #진입차수가 0인 곳부터 시작!
+        heapq.heappush(hq, i)
+ans = []
+
+while hq:
+    cur = heapq.heappop(hq)
+    ans.append(cur)
+
+    for nxt in lst[cur]:
+        con[nxt] -= 1 #연결되어 있는 간선 모두 끊기
+        if con[nxt] == 0: #진입차수가 모두 0이 되었으면 큐에 추가
+            heapq.heappush(hq, nxt)
+
+print(*ans)
+
+
+
