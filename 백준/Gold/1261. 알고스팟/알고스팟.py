a9af0c1a08cf6554bb609c01216dd19434c120c3
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-10-06 15:48:39 +0900

    [Gold IV] Title: 알고스팟, Time: 56 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.py
+++ 백준/Gold/1261. 알고스팟/알고스팟.py
@@ -1,39 +1,36 @@
 '''
-2025.10.5
+2025.10.6
+다익스트라
 '''
 import sys
-from collections import deque
+import heapq
 input = sys.stdin.readline
 
 dr = [0,1,0,-1]
 dc = [1,0,-1,0]
 
-def bfs(r,c):
-    q = deque()
-    q.append((r,c,0)) #부순 벽 갯수
-    visit = [[False]*(m) for _ in range(n)]
-    visit[r][c] = True
-    ans = -1
-    while q:
-        cr,cc,cnt = q.popleft()
-        if (cr,cc) == (n-1,m-1):#도착
-            ans = cnt
+def dijkstra():
+
+    hq = []
+    heapq.heappush(hq, (0,0,0))
+    distance[0][0] = 0 #첫 시작점 거리 0
+
+    while hq:
+        cost,cr,cc = heapq.heappop(hq)
+
+        if cost > distance[cr][cc]: #더 최소 벽 깨는게 아니면, 패스
+            continue
 
         for i in range(4):
             nr,nc = cr+dr[i], cc+dc[i]
-            if 0<=nr<n and 0<=nc<m and visit[nr][nc] == False:
-                if board[nr][nc] == 0: #빈 방이면, 우선적으로 돌도록 appendleft!!
-                    q.appendleft((nr,nc,cnt))
-                else:
-                    q.append((nr,nc,cnt+1))
-                visit[nr][nc] = True
-    return ans
-
+            if 0<=nr<n and 0<=nc<m: #범위 안이면,
+                if cost+lst[nr][nc] < distance[nr][nc]: #더 최소 벽 깨기면,
+                    distance[nr][nc] = cost+lst[nr][nc]
+                    heapq.heappush(hq,(distance[nr][nc],nr,nc))
 
 m,n = map(int,input().split())
-board = []
-for _ in range(n):
-    temp = list(map(int,input().strip()))
-    board.append(temp)
+lst = [list(map(int,input().strip())) for _ in range(n)]
+distance = [[1e10]*m for _ in range(n)]
 
-print(bfs(0,0))
+dijkstra()
+print(distance[n-1][m-1])
