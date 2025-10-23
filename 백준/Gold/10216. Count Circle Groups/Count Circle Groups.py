a6b86480ecec7be55e276cc90f12730aa78413ca
Yoonkyung Ra <114200656+raours@users.noreply.github.com>
2025-10-23 15:32:03 +0900

    [Gold IV] Title: Count Circle Groups, Time: 548 ms, Memory: 136368 KB -BaekjoonHub

--- 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
@@ -1,36 +1,55 @@
 import sys
-sys.setrecursionlimit(10**5)
-# 1. 입력 받기
 input = sys.stdin.readline
 
 def find(x):
-    if parent[x] != x:
-        parent[x] = find(parent[x])
-    return parent[x]
+    # iterative + path halving
+    while parent[x] != x:
+        parent[x] = parent[parent[x]]
+        x = parent[x]
+    return x
 
+def union(a, b):
+    ra, rb = find(a), find(b)
+    if ra == rb:
+        return
+    if size[ra] < size[rb]:
+        ra, rb = rb, ra
+    parent[rb] = ra
+    size[ra] += size[rb]
 
-def union(x,y):
-    x = find(x)
-    y = find(y)
-
-    if x<y:
-        parent[y] = x
-    else:
-        parent[x] = y
-
-t = int(input())
-for i in range(1,t+1):
+T = int(input())
+out = []
+for _ in range(T):
     n = int(input())
-    lst = [list(map(int, input().split())) for _ in range(n)]
-    parent = [k for k in range(n)]
-    for a in range(n):
-        x1, y1, r1 = lst[a]
-        for b in range(a+1,n):
-            x2, y2, r2 = lst[b]
+    pts = [tuple(map(int, input().split())) for _ in range(n)]  # (x,y,r)
 
-            if (x1-x2)**2 + (y1-y2)**2 <= (r1+r2)**2: #범위 내이면,
-                union(a,b)
+    parent = list(range(n))
+    size = [1]*n
 
-    for num in range(n):
-        find(num)
-    print(len(set(parent)))
+    # x정렬 + 안전한 조기 종료를 위한 suffix max r
+    order = sorted(range(n), key=lambda i: pts[i][0])
+    xs = [pts[i][0] for i in order]
+    rs = [pts[i][2] for i in order]
+    suf_max_r = [0]*n
+    suf_max_r[-1] = rs[-1]
+    for k in range(n-2, -1, -1):
+        suf_max_r[k] = rs[k] if rs[k] > suf_max_r[k+1] else suf_max_r[k+1]
+
+    for ii in range(n):
+        i = order[ii]
+        x1, y1, r1 = pts[i]
+        for jj in range(ii+1, n):
+            dx = xs[jj] - x1
+            if dx > r1 + suf_max_r[jj]:   # 안전한 조기 종료
+                break
+            j = order[jj]
+            x2, y2, r2 = pts[j]
+            dy = y2 - y1
+            s = r1 + r2
+            if dx*dx + dy*dy <= s*s:
+                union(i, j)
+
+    groups = len({find(i) for i in range(n)})
+    out.append(str(groups))
+
+print("\n".join(out))
